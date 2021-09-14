71dd3fd76d9039c94fd9db4fb10697df2a2f7624
jorgemgmb <65348948+jorgemgmb@users.noreply.github.com>
2021-09-14 23:26:06 +0200

    Basico v2
    Versión para que funcione en windows WSL

--- code/exec.py
+++ code/exec.py
@@ -5,14 +5,23 @@ from data import HascatJSON
 
 def execHashcat (data, mask):
 
+    #-------------------- COMANDO EJECUCIÓN MAC --------------------------------------
     #hashcat -m 0 -a 3 --runtime=60 --status-json --session=[mascara para identificar] wordlists/wordlist1MD5.txt [mascara]
+    '''
     result = subprocess.run(['hashcat', '-m' ,'0', '-a', '3', '--runtime=60', '--status-json', '--session={:s}'.format(mask), 
     'wordlists/wordlist1MD5.txt', mask], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    '''
+    
+    #-------------------- COMANDO EJECUCIÓN WINDOWS WSL -----------------------------------
+    #./hashcat.exe -m 0 -a 3 --runtime=60 --status-json ../wordlists/wordlist1MD5.txt ?d?d?d?d
+    result = subprocess.run(['./hashcat.exe',  '-m' ,'0', '-a', '3', '--runtime=60', '--status-json', '--session={:s}'.format(mask.replace('?', '_')), 
+    '../wordlists/wordlist1MD5.txt', mask], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
     # RegEX para encontrar el JSON dentro de la salida obtenida de HASHACAT == grep '{.*}'
     reg1 = regex.compile('{.*}')
     
     filteredResult = reg1.search(result)
+    
     resultJSON = json.loads(filteredResult[0])
 
     data.setJSON(resultJSON)
--- code/main.py
+++ code/main.py
@@ -8,14 +8,14 @@ from exec import execHashcat
 if __name__=="__main__":
     
     dataList = HascatJSON()
-    masks = ['?d?d?d?d', '?l?l?l?l']
+    masks = ['?d?d?d?d?d?d', '?l?l?l?l']
 
     for mask in masks:
         execHashcat(dataList, mask)
         # Eliminar este archivo para que haga el proceso desde 0 con cada máscara, sino recovered_hashes tiene el valor total que se han 
-        # recuperado con todas las máscaras usadas
-        if os.path.isfile('~/.hashcat/hashcat.potfile'):
-            subprocess.run(['rm', '~/.hashcat/hashcat.potfile'])
+        # recuperado con todas las máscaras usadas (SOLO BORRA EN WINDOWS DE MOMENTO)
+        if os.path.isfile('hashcat.potfile'):
+            subprocess.run(['rm', 'hashcat.potfile'])
     
     dataFrame = pd.DataFrame.from_dict(dataList.getJSON())
 

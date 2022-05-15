6cd9b0fe3fdd1338871f431076599e6e9dd961dc
Jorge Martinez Garcia <jorgemartinezgarciamb@gmail.com>
2022-05-15 20:50:21 +0000

    Experimentos servidor

--- DOWNLOAD/code/main.py
+++ DOWNLOAD/code/main.py
@@ -10,9 +10,9 @@ fin = datetime.datetime.now()
 
 if maskcat_config.OUTPUT_FILES:
     fd = open ("{}/times/times.txt".format(maskcat_config.DIRECTORY_OUTPUT_FILES), "w")
-    fd.write('''Experimento : maskcat normal poblacion tamaño 100 y 100 generaciones
+    fd.write('''Experimento : prueba rendimiento maskcat poblacion 100, 100 generacines 30 iteraciones
     Inicio: {}
-    Fin: {}'''.format(inicio, fin))
+    Fin: {}\n'''.format(inicio, fin))
     fd.close()
 
 print("Hora de inicio= {}\nHora de fin= {}\n".format(inicio, fin))
--- DOWNLOAD/code/maskcat_config.py
+++ DOWNLOAD/code/maskcat_config.py
@@ -4,16 +4,22 @@ import datetime
 
 #========== Directories to open or write files ==========
 # Generar ficheros para analizar la ejecución
-OUTPUT_FILES = False
-DIRECTORY_OUTPUT_FILES = ".\experiments\maskcat_no-output_{}".format(datetime.date.today())
+OUTPUT_FILES = True
+#DIRECTORY_OUTPUT_FILES = "./experiments/maskcat_prueba_{}".format(datetime.date.today())
+DIRECTORY_OUTPUT_FILES = "./experiments/maskcat_loop100"
 
 #Preferible ruta absoluta del fichero wordlist
-WORDLIST_ROUTE = "C:\\Users\\Jorge\\Desktop\\TFG\\maskcat\\DOWNLOAD\\wordlists\\shuffled_top_1M_MD5.txt"
+#WORDLIST_ROUTE = "C:\\Users\\Jorge\\Desktop\\TFG\\maskcat\\DOWNLOAD\\wordlists\\shuffled_top_1M_MD5.txt"
+
+WORDLIST_ROUTE = "/home/alumno/maskcat/DOWNLOAD/wordlists/shuffled_top_1M_MD5.txt"
+
 
 #========== Maskcat Command ==========
 # Modificar los valores a corde al comando que se quiera ejecutar, no introducir máscara (se hace en hashcat_exec.py), ruta de wordlist se sextrae de
 # la variable definida con anterioridad
-HASHCAT_COMMAND = "hashcat.cmd -m 0 -a 3 -d 1 --runtime=600 --status-json -O --potfile-disable --logfile-disable {}".format(WORDLIST_ROUTE)
+#HASHCAT_COMMAND = "hashcat.cmd -m 0 -a 3 -d 1 --runtime=600 --status-json -O --potfile-disable --logfile-disable {}".format(WORDLIST_ROUTE)
+
+HASHCAT_COMMAND = "NONE"
 
 #========== Maskcat problem execution variables ==========
 #MASK_LEN = Longitud de la máscara - 1
@@ -26,8 +32,8 @@ GENERATIONAL = False
 POPULATION_RESET = False
 POPULATION_RESET_NUMBER = 1
 
-POPULATION_SIZE=50
-OFFSPRING_POPULATION = 50
-MAX_EVALUATIONS=5000
-REPETITIONS=1
+POPULATION_SIZE=100
+OFFSPRING_POPULATION = 100
+MAX_EVALUATIONS=10000
+REPETITIONS=30
 

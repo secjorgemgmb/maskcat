ecccc1b5b7a6bab173985263868b777b8a983b2c
Jorge Martinez Garcia <jorgemartinezgarciamb@gmail.com>
2022-05-27 06:21:53 +0000

    Experimentos servidor 26 mayo

--- DOWNLOAD/code/main.py
+++ DOWNLOAD/code/main.py
@@ -10,9 +10,12 @@ fin = datetime.datetime.now()
 
 if maskcat_config.OUTPUT_FILES:
     fd = open ("{}/times/times.txt".format(maskcat_config.DIRECTORY_OUTPUT_FILES), "w")
-    fd.write('''Experimento : prueba rendimiento maskcat poblacion 50, 100 generacines algortimo generacional, bucle 10 iteraciones
+    fd.write('''Experimento : maskcat (generational = {})
+    Poblacion = {}
+    Num generaciones = {}
+    Num iteraciones = {}
     Inicio: {}
-    Fin: {}\n'''.format(inicio, fin))
+    Fin: {}\n'''.format(maskcat_config.GENERATIONAL, maskcat_config.POPULATION_SIZE,(maskcat_config.MAX_EVALUATIONS / maskcat_config.POPULATION_SIZE), maskcat_config.REPETITIONS ,inicio, fin))
     fd.close()
 
 print("Hora de inicio= {}\nHora de fin= {}\n".format(inicio, fin))
--- DOWNLOAD/code/maskcat_config.py
+++ DOWNLOAD/code/maskcat_config.py
@@ -6,7 +6,7 @@ import datetime
 # Generar ficheros para analizar la ejecución
 OUTPUT_FILES = True
 #DIRECTORY_OUTPUT_FILES = "./experiments/maskcat_prueba_{}".format(datetime.date.today())
-DIRECTORY_OUTPUT_FILES = "./experiments/maskcat_loop50-100_generational"
+DIRECTORY_OUTPUT_FILES = "./experiments/maskcat_loop300-100"
 
 #Preferible ruta absoluta del fichero wordlist
 #WORDLIST_ROUTE = "C:\\Users\\Jorge\\Desktop\\TFG\\maskcat\\DOWNLOAD\\wordlists\\shuffled_top_1M_MD5.txt"
@@ -27,13 +27,13 @@ MASK_LEN=7
 PREDEFINED_MASKS=0
 
 #========== Maskcat GeneticAlgorithm execution variables ==========
-GENERATIONAL = True
+GENERATIONAL = False
 
 POPULATION_RESET = False
 POPULATION_RESET_NUMBER = 1
 
-POPULATION_SIZE=50
-OFFSPRING_POPULATION = 50
-MAX_EVALUATIONS=5000
+POPULATION_SIZE=300
+OFFSPRING_POPULATION = 300
+MAX_EVALUATIONS=30000
 REPETITIONS=10
 

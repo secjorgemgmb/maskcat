8bbda896419723096285ab65e3262e0398412351
secjorgemgmb <65348948+secjorgemgmb@users.noreply.github.com>
2022-06-21 13:13:44 +0200

    Update maskcat.sh

--- MASKCAT/maskcat.sh
+++ MASKCAT/maskcat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 code/main.py 200 100 10 2> log_200_100_10.txt
+#python3 code/main.py 200 100 10 2> log_200_100_10.txt
 
 python3 code/main.py 200 100 30 2> log_200_100_30.txt
 
